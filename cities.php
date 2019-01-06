180b2d234e8b8019935e133cb38734363e06c90c
Gedridze <edvinas.kilciauskas@gmail.com>
2019-01-06 18:23:12 +0200

    Separated logic code fragments to corresponding controller files

--- cities.php
+++ cities.php
@@ -5,47 +5,7 @@
 </head>
 <body>
 <!--Duombazes prisijungimo parametrai nustatyti config.php faile-->
-<?php
-include ('database.php');
-include('cityModel.php');
-include('countryModel.php');
-$database = new Database();
-$countryModel = new countryModel($database->connection);
-$cityModel = new cityModel($database->connection);
-
-
-if(isset($_GET['page'])){
-    $pageno = $_GET['page'];
-}
-else{
-    $pageno = 1;
-}
-$country_id = $_GET['id'];
-$no_per_page = config::RECORD_NUM_PER_PAGE; // Nustatoma kiek irasu norima matyti viename puslapyje
-    if(isset($_GET['search_string'])){
-        $search_string = $_GET['search_string']; // issaugoma paieskos lauko reiksme
-        $link = "?id=".$country_id."&search_string=".$_GET['search_string']."&"; //$link - kintamasis skirtas saugoti GET parametru reiksmes
-                                                                                 //kad puslapiuojant jos nedingtu
-        $total_pages = ceil($cityModel->getSearchedCitiesCount($country_id, $_GET['search_string']) / $no_per_page);
-        $cities = $cityModel->searchCities($country_id, $_GET['search_string'], $pageno, $no_per_page);
-    }
-    else {
-        $search_string = "";
-        $link = "?id=".$country_id."&";
-        $total_pages = ceil($cityModel->getCitiesCount($_GET['id']) / $no_per_page);
-        $cities = $cityModel->getCities($country_id, $pageno, $no_per_page);
-    }
-
-if($pageno > $total_pages && $total_pages > 0){
-    header('Location: cities.php?id='.$country_id.'&page='.$total_pages); //Jei URL irasomas per didelis puslapio numeris - peradresuojama i paskutini puslapi
-    exit();
-}
-else if($pageno < 1) {
-    header('Location: cities.php?id='.$country_id.'&page=1'); //Jei URL irasomas per mazas puslapio numeris - peradresuojama i pirma puslapi
-    exit();
-}
-    $country = $countryModel->getCountry($country_id); // salis, kurios miestai atvaizduojami
-?>
+<?php include 'cityController.php'; ?>
 <div class="container">
     <?php if(!empty($_SESSION['errors'])){?>
         <div class="errors">
--- countries.php
+++ countries.php
@@ -8,49 +8,7 @@
     <link type="text/css" rel="stylesheet" href="style.css">
 </head>
 <body>
-<?php include ('database.php');
-      include('countryModel.php');
-$database = new Database();
-$countryModel = new countryModel($database->connection);
-if(isset($_GET['page'])){
-    $pageno = $_GET['page'];
-}
-else{
-    $pageno = 1;
-}
-$no_per_page = config::RECORD_NUM_PER_PAGE;
-
-if(isset($_GET['search_string'])){
-    $search_string = $_GET['search_string'];
-    $link = "?search_string=".$_GET['search_string']."&";
-    $total_pages = ceil($countryModel->getSearchedCountriesCount($_GET['search_string']) / $no_per_page);
-    $countries = $countryModel->searchCountries($_GET['search_string'], $pageno, $no_per_page);
-    if($pageno > $total_pages && $total_pages > 0){
-        header('Location: countries.php'.$link.'page='.$total_pages);
-        exit();
-    }
-    else if($pageno < 1) {
-        header('Location: countries.php'.$link.'page=1');
-        exit();
-    }
-}
-else {
-    $search_string = "";
-    $link = "?";
-    $total_pages = ceil($countryModel->getCountriesCount() / $no_per_page);
-    $countries = $countryModel->getCountries($pageno, $no_per_page);
-    if($pageno > $total_pages && $total_pages > 0){
-        header('Location: countries.php?page='.$total_pages);
-        exit();
-    }
-    else if($pageno < 1) {
-        header('Location: countries.php?page=1');
-        exit();
-    }
-}
-
-
-?>
+<?php include 'countryController.php';?>
 <div class="container">
     <?php if(!empty($_SESSION['errors'])){?>
     <div class="errors">
